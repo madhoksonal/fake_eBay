d61377c8741da67c5a06a6d78f8fade35046baaa
Kateri Arano <79236631+itsKateri@users.noreply.github.com>
2021-03-31 21:51:02 -0400

    Update fakeEbay.sql

--- fakeEbay.sql
+++ fakeEbay.sql
@@ -1,105 +1,60 @@
-CREATE DATABASE  IF NOT EXISTS `fakeEbay` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `fakeEbay`;
 USE `fakeEbay`;
--- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
---
--- Host: localhost    Database: fakeEbay
--- ------------------------------------------------------
--- Server version	8.0.23
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `accounts`
+-- Creating tables for accounts, admin, and customer support
 --
 
 DROP TABLE IF EXISTS `accounts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `accounts` (
   `login_name` varchar(20) NOT NULL DEFAULT '',
   `email` varchar(50) NOT NULL DEFAULT '',
   `password` varchar(30) NOT NULL DEFAULT '',
   `upper_bid_limit` decimal(9,2) DEFAULT NULL,
   PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `accounts`
---
-
-LOCK TABLES `accounts` WRITE;
-/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES ('eeyore','donkeyboy@hotmail.com','33y0r3',NULL),('newUser1','newEmail1','newPW1',NULL),('newUser2','newEmail2','newPW2',NULL),('piglet','pinkpig@yahoo.com','b3stfri3nd',NULL),('chrisrobin','robinchris@gmail.com','fr!endsh!p',NULL),('winnie','wtp@gmail.com','h0ney',NULL);
-/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `adminStaff`
---
+-- need to implement account ID? temporarily using email as pk
 
 DROP TABLE IF EXISTS `adminStaff`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `adminStaff` (
   `login_name` varchar(20) NOT NULL DEFAULT '',
   `email` varchar(50) NOT NULL DEFAULT '',
   `password` varchar(30) NOT NULL DEFAULT '',
   PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `adminStaff`
---
-
-LOCK TABLES `adminStaff` WRITE;
-/*!40000 ALTER TABLE `adminStaff` DISABLE KEYS */;
-INSERT INTO `adminStaff` VALUES ('admin36','admin@admin.auction.com','cs336');
-/*!40000 ALTER TABLE `adminStaff` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `customerRep`
---
+);
 
 DROP TABLE IF EXISTS `customerRep`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `customerRep` (
   `login_name` varchar(20) NOT NULL DEFAULT '',
   `email` varchar(50) NOT NULL DEFAULT '',
   `password` varchar(30) NOT NULL DEFAULT '',
   PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
 --
--- Dumping data for table `customerRep`
+-- inserting adminStaff and customerRep manually
 --
 
-LOCK TABLES `customerRep` WRITE;
-/*!40000 ALTER TABLE `customerRep` DISABLE KEYS */;
-INSERT INTO `customerRep` VALUES ('support_fauna','fairygodmother@support.auction.com','greenFairy'),('support_merryweather','guardian@support.auction.com','blueFairy'),('support_flora','supportfairy@support.auction.com','redFairy');
-/*!40000 ALTER TABLE `customerRep` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+insert into adminStaff values
+('admin36','admin@admin.auction.com', 'cs336');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+insert into customerRep values
+('support_flora','supportfairy@support.auction.com', 'redFairy'),
+('support_fauna','fairygodmother@support.auction.com', 'greenFairy'),
+('support_merryweather','guardian@support.auction.com', 'blueFairy');
 
--- Dump completed on 2021-03-23 21:25:19
+--
+-- Populating Accounts With Data to Test With
+--
+
+insert into accounts values
+('winnie','wtp@gmail.com', 'h0ney', NULL),
+('piglet','pinkpig@yahoo.com', 'b3stfri3nd', NULL),
+('eeyore','donkeyboy@hotmail.com', '33y0r3', NULL),
+('chrisrobin','robinchris@gmail.com', 'fr!endsh!p', NULL);
+
+-- Queries for testing
+select * from accounts;
+select * from adminStaff;
+select * from customerRep;
